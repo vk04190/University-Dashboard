97d73c0ed47a0da2e365fc0fa1649a50d324150a
Vivek Kumar <er.vivekkumar21@gmail.com>
2017-10-26 01:33:10 +0530

    All Initial Setup Completed

--- insta_app/models.py
+++ insta_app/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=200)
+    phone = models.CharField(max_length=30)
+    verified_phone_no = models.BooleanField(default=False)
+    age = models.IntegerField(default=0)
+    created_on = models.DateField(auto_now_add=True)
